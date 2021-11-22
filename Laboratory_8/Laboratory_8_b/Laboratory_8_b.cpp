b0f42a5e0fc70c40907e22c28631085bcaeda615
Дар`я Автенюк <Daria.Avteniuk@cs.khpi.edu.ua>
2021-11-22 18:26:03 +0200

    done

--- Laboratory_8/Laboratory_8_b/Laboratory_8_b.cpp
+++ Laboratory_8/Laboratory_8_b/Laboratory_8_b.cpp
@@ -1,20 +1,147 @@
-﻿// Laboratory_8_b.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+#include <algorithm>
+
+#include <iomanip>
+
+#include <string>
+
+using namespace std;
+
+
+
 
 int main()
+
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+    int arr_size;
+    cout << "Введите количество элементов массива: ";
+    cin >> arr_size;
+    int* arr = new int[arr_size];
+    int k; // левые эл-ты
+    int i; // индекс 1го мн-ва
+    int j; // индекс 2го мн-ва
+    // магичим рандомность
+    cout << "Ваш массив:" << endl;
+    for (i = 0; i < arr_size; i++) {
+
+        arr[i] = rand() % 10 + 1;
+
+        cout << arr[i] << setw(4);
+
+    }
+    cout << endl;
+    // условьице
+
+    string answer;
+
+    string del_new("del");
+
+    string add_new("add");
+
+    while (true) {
+
+        cout << "Вы хотите удалить или добавить элементы? <del/add> ";
+
+        cin >> answer;
+        // добавляем
+
+        if (answer == add_new) {
+
+            // вводим
+
+            int pos; //для точности посыла
+
+            cout << "Сколько элементов вы хотите добавить? ";
+
+            cin >> k;
+
+            cout << "Выберите позицию, с которой вы хотите добавть данное количество элементов: " << k << endl;
+
+            cin >> pos;
+            // add loopы
+            int* add_new_arr = new int[arr_size + k];
+            for (i = 0; i < pos; i++) {
+
+                add_new_arr[i] = arr[i];
+
+            }
+            int* add_elements = new int[k];
+
+            cout << "Новые элементы: ";
+
+            for (j = 0; j < k; j++) {
+
+                add_elements[j] = rand() % 10 + 1;
+
+                cout << add_elements[j] << setw(4);
+
+            }
+            cout << endl;
+            for (j = 0, i = pos; i < arr_size + k; i++, j++) {
+
+                add_new_arr[i] = add_elements[j];
+
+            }
+            for (j = pos + k, i = pos; j < arr_size + k; j++, i++) {
+
+                add_new_arr[i + k] = arr[i];
+
+            }
+            // массив 2.0
+
+            cout << "Ваш новый массив:" << endl;
+
+            for (i = 0; i < arr_size + k; i++) {
+
+                cout << add_new_arr[i] << setw(4);
+
+            }
+
+            cout << endl;
+        }
+
+        // удаляем
+
+        else if (answer == del_new) {
+            // вводим
+
+            cout << "Сколько элементов вы хотите удалить? ";
+
+            cin >> k;
+            cout << "Выберите позицию, с которой вы хотите удалить данное количество элементов: " << k << endl;
+
+            cin >> i;
+            if (arr_size == k and i == 0) {
+
+                cout << "Массив удалён!" << endl;
+
+            }
+            else {
+                for (; i < arr_size - k; i++) {
+
+                    arr[i] = arr[i + k];
+
+                }
+              // массив 2.0
+
+                cout << "Ваш новый массив:" << endl;
+
+                for (i = 0; i < arr_size - k; i++) {
+
+                    cout << arr[i] << setw(4);
+
+                }
+
+                cout << endl;
+
+            }
+
+        }
+
+    }
+
+    return 0;
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Laboratory_8/Laboratory_8_c/Laboratory_8_c.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<vector>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int num;
+    int reverse = 0;
+    int sum = 0;
+    int array_size = 0;
+    int hash;
+    cout << "Введите свое число: ";
+    cin >> num;
+    hash = num;
+    do {
+        hash /= 10;
+        array_size++;
+    } while (hash > 0);
+    int* array = new int[array_size];
+    int i = 0;
+    for (i = 0; i < array_size; i++) {
+        array[i] = (num % 10);
+        reverse = reverse * 10 + array[i];
+        num /= 10;
+        sum += array[i];
+    }
+    cout << "Число наоборот: " << reverse;
+    cout << endl;
+    cout << "Сумма: " << sum << endl;
+    cout << "Всего чисел: " << i << endl;
+    return 0;
+}
