bb3d29c34cc708cc0e51bb81337ca0461dffec98
Дар`я Автенюк <Daria.Avteniuk@cs.khpi.edu.ua>
2021-11-27 11:13:54 +0200

    done

--- Laboratory_9/Laboratory_9_a/Laboratory_9_a.cpp
+++ Laboratory_9/Laboratory_9_a/Laboratory_9_a.cpp
@@ -1,1 +1,82 @@
-﻿
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+#pragma warning(disable : 4996)
+#include<iomanip>
+#define M 5
+
+int main()
+
+{
+    setlocale(LC_ALL, "Russian");
+
+    int Ar[M][M];
+    int MS[M][M];
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            Ar[i][j] = rand() % 10 + 1;
+            MS[i][j] = Ar[i][j];
+            cout << setw(4) << Ar[i][j];
+        }
+        cout << endl;
+    }
+    int max = 0;
+    int temp = 0;
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if (Ar[i][j] >= Ar[i][max])
+                max = j;
+
+        }
+        cout << "Максимальное значение в строке:" << Ar[i][max] << endl;
+
+        temp = Ar[i][0];
+        Ar[i][0] = Ar[i][max];
+        Ar[i][max] = temp;
+        max = 0;
+    }
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cout << setw(4) << Ar[i][j];
+        }
+        cout << endl;
+    }
+    cout << "Исходный массив:\n";
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cout << setw(4) << MS[i][j];
+        }
+        cout << endl;
+    }
+    int min = 0;
+    int change = 0;
+    for (int j = 0; j < M; j++)
+    {
+        for (int i = 0; i < M; i++) {
+            if (MS[i][j] <= MS[min][j])
+                min = i;
+        }
+        cout << "Минимальное значение столбца:" << MS[min][j] << endl;
+        change = MS[M - 1][j];
+        MS[M - 1][j] = MS[min][j];
+        MS[min][j] = change;
+    }
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cout << setw(4) << MS[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ Laboratory_9/Laboratory_9_b/Laboratory_9_b.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+#pragma warning(disable : 4996)
+#include<iomanip>
+#define M 5
+
+int main()
+
+{
+    setlocale(LC_ALL, "Russian");
+    int razmerstr = 0;
+    int razmerstolb = 0;
+    int Ar[M][M];
+    int Ms[M][M];
+    cout << "Введите количество строк матрицы 1" << endl;
+    cin >> razmerstr;
+    cout << "Введите количество столбцов матрицы 1" << endl;
+    cin >> razmerstolb;
+    int sizerow = 0;
+    int sizecol = 0;
+    cout << "Введите количество строк матрицы 2" << endl;
+    cin >> sizerow;
+    cout << "Введите количество столбцов матрицы 2" << endl;
+    cin >> sizecol;
+
+    while (razmerstr != sizecol or razmerstolb != sizerow) {
+        cout << "Такое перемножение матриц невозможно" << endl << "Попытайтесь еще" << endl;
+        cout << "Введите количество строк матрицы 1" << endl;
+        cin >> razmerstr;
+        cout << "Введите количество столбцов матрицы 1" << endl;
+        cin >> razmerstolb;
+        cout << "Введите количество строк матрицы 2" << endl;
+        cin >> sizerow;
+        cout << "Введите количество столбцов матрицы 2" << endl;
+        cin >> sizecol;
+    }
+    cout << "Ваши матрицы:" << endl;
+    for (int i = 0; i < razmerstr; i++)
+    {
+        for (int j = 0; j < razmerstolb; j++)
+        {
+
+
+            Ar[i][j] = rand() % 15;
+            cout << setw(4) << Ar[i][j];
+        }
+        cout << endl;
+    }
+    cout << "\n\n";
+    for (int i = 0; i < sizerow; i++)
+    {
+        for (int j = 0; j < sizecol; j++)
+        {
+            Ms[i][j] = rand() % 15;
+            cout << setw(4) << Ms[i][j];
+        }
+        cout << endl;
+    }
+    cout << "\n\n";
+    int proizv = 0;
+    int sum = 0;
+    int umn = 0;
+    int sloz = 0;
+    int mass[M][M];
+    if (razmerstr == sizecol or razmerstolb == sizerow) {
+        for (int k = 0; k < razmerstr; k++)
+            for (int i = 0; i < razmerstr; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < sizerow; j++) {
+
+                    proizv = Ar[k][j] * Ms[j][i];
+                    sum += proizv;
+                    mass[k][i] = sum;
+                    cout << "Результат перемножения:" << proizv << endl;
+                    cout << "Результат сложения:" << sum << endl;
+
+                }
+            }
+    }
+    cout << "Ваши перемноженные матрицы:" << endl;
+    for (int k = 0; k < razmerstr; k++)
+    {
+        for (int i = 0; i < sizecol; i++)
+        {
+            cout << setw(4) << mass[k][i];
+        }
+        cout << endl;
+    }
+}
