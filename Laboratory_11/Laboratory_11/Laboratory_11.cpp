b8b5e307d546caeb6bd4583b20147874ecbbd6f1
Дар`я Автенюк <Daria.Avteniuk@cs.khpi.edu.ua>
2022-02-09 13:38:42 +0200

    done

---
+++ Laboratory_11/Laboratory_11/Laboratory_11.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+const int n = 100;
+int main()
+{
+    int i = 0, arr[n];
+    setlocale(LC_ALL, "rus");
+    cout << "Введите последовательность чисел:";
+    do
+    {
+        cin >> arr[i++];
+
+    } 
+    while (arr[i - 1] != 0);
+    int size = i;
+    cout << "В обратном порядке:";
+    for (int i = size - 1; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    };
+    system("pause>>void");
+    return 0;
+}
