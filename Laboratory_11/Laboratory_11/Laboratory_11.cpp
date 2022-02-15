12a81aa626407dc90b788db45d09bb227ad06d6c
Дар`я Автенюк <Daria.Avteniuk@cs.khpi.edu.ua>
2022-02-15 20:27:42 +0200

    done

--- Laboratory_11/Laboratory_11/Laboratory_11.cpp
+++ Laboratory_11/Laboratory_11/Laboratory_11.cpp
@@ -1,23 +1,25 @@
 ﻿#include <iostream>
 using namespace std;
-const int n = 100;
+# define n 100;
+void sort(int arr[], int size)
+{ for (int i = size - 1; i >= 0; i--)
+{
+    cout << arr[i] << " ";
+};
+}
 int main()
 {
-    int i = 0, arr[n];
+    int i = 0, arr[100];
     setlocale(LC_ALL, "rus");
     cout << "Введите последовательность чисел:";
     do
     {
         cin >> arr[i++];
 
-    } 
+    }
     while (arr[i - 1] != 0);
     int size = i;
     cout << "В обратном порядке:";
-    for (int i = size - 1; i >= 0; i--)
-    {
-        cout << arr[i] << " ";
-    };
-    system("pause>>void");
+    sort(arr, size);
     return 0;
 }
--- Laboratory_1_2t/Laboratory_1_2t/Laboratory_1_2t.cpp
+++ Laboratory_1_2t/Laboratory_1_2t/Laboratory_1_2t.cpp
@@ -1,115 +1,119 @@
-﻿#include <iostream>
-#include <stdio.h>
-#define _CRT_SECURE_NO_WARNINGS
-#define M 3
-#pragma warning(disable : 4996)
+﻿#include <iostream> 
+#include <string.h> 
+#include <stdio.h> 
+#define _CRT_SECURE_NO_WARNINGS 
+#define M 3 
+#pragma warning(disable : 4996) 
 using namespace std;
 struct mon {
-	char name[15]; /* название */
-	char sc;       /* группа */
-	int cnt;       /* место обитания */
-	float sq;      /* численность */
-} /*mm[10]*/;
-void input(mon mm[]) {
-	int n = M; //количество элементов в массиве
-	int i, j;// текущие индексы в массиве
-	int m;//индекс минимального элемента
-	float sqx;//рабочая переменная
-	for (n = 0; n < M; n++)
-	{
-		printf("%d. Введите: название, группу, место обитания, численность >",
-			n + 1);
-		scanf_s("%s", mm[n].name, sizeof(mm[n].name));
-		if (!strcmp(mm[n].name, "***")) break;
-		scanf_s("%c", &mm[n].sc, sizeof(mm[n].name));
-		scanf_s("%s", &mm[n].cnt);
-		//обходим ошибку в системе программирования
-		scanf_s("%i", &mm[n].sq);//mm[n].sq = sqx;
-	}
-}
-void print(mon mm[]) {
+    char name[8];
+    char sc;
+    char cnt[8];
+    float sq;
+    int mas[10];
+};
+void input( mon mm[]) {
+    int n = M;
+    int i, j;
+    int m;
 
-	int n = M; //количество элементов в массиве
-	int i, j;// текущие индексы в массиве
-	int m;//индекс минимального элемента
-	struct mon x;//рабочая переменная
-	printf("-----------------------------------------------\n");
-	printf("|         Некоторые виды антилоп              |\n");
-	printf("|---------------------------------------------|\n");
-	printf("| Название | Группа |   Место  | Численность  |\n");
-	printf("|          |        | обитания |  популяции   |\n");
-	printf("|----------|--------|----------|--------------|\n");
-	for (i = 0; i < n; i++)
-	{
-		printf("|%-10s|%-8c|%-10s|%-14d|\n",
-			mm[i].name, mm[i].sc, mm[i].cnt, mm[i].sq);
-	}
-	printf("|---------------------------------------------|\n");
+    for (n = 0; n < M; n++)
+    {
+        printf("%d. Введите: название, группу, место обитания, численность >",
+            n + 1);
+        scanf_s("%s", mm[n].name, sizeof(mm[n].name));
+        if (!strcmp(mm[n].name, "***")) break;
+        scanf_s("%c", &mm[n].sc, sizeof(mm[n].name));
+        scanf_s("%s", &mm[n].cnt, sizeof(mm[n].name));
+        scanf_s("%f", &mm[n].sq);
+    }
+}
+void print( mon mm[]) {
+
+    int n = M;
+    int i, j;
+
+
+    printf("-----------------------------------------------\n");
+    printf("|         Некоторые виды антилоп              |\n");
+    printf("|---------------------------------------------|\n");
+    printf("| Название | Группа |   Место  | Численность  |\n");
+    printf("|          |        | обитания |  популяции   |\n");
+    printf("|----------|--------|----------|--------------|\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("|%-10s|%-8c|%-10s|%-14f|\n",
+            mm[i].name, mm[i].sc, mm[i].cnt, mm[i].sq);
+    }
+    printf("|---------------------------------------------|\n");
 
 
 }
 void sort(mon mm[]) {
-	int m;
-	int n = M;
-	struct mon x;
-	for (int i = 0; i < n - 1; i++) {
-		m = i; /* минимальный элемент - первый */
-		for (int j = i + 1; j < n; j++)
-			/* если текущий элемент > минимального,
-			   он становится минимальным */
-			if (strcmp(mm[m].name, mm[j].name) > 0) m = j;
-		if (m > i) {
-
-			x = mm[i];
-			mm[i] = mm[m];
-			mm[m] = x;
-		}
-	}
+    int m;
+    int n = M;
+    struct mon x;
+    for (int i = 0; i < n - 1; i++) {
+        m = i;/* минимальный элемент - первый */
+        for (int j = i + 1; j < n; j++)
+            /* если текущий элемент > минимального, он становится минимальным */
+            if (strcmp(mm[m].name, mm[j].name) > 0) m = j;
+        if (m > i) {
+            //перестановка 
+            x = mm[i];
+            mm[i] = mm[m];
+            mm[m] = x;
+        }
+    }
 }
-void random(mon mm[]) {
-	for (int n = 0; n < M; n++) {
-		printf("%d. Введите название>", n + 1);
-		scanf_s("%s", mm[n].name, sizeof(mm[n].name));
-	}
-		char letters[3] = { 'A', 'B' ,'H' };
-		for (int i = 0; i < 3; i++)
-		{
-			mm[i].cnt = rand() % 40 + (40 * (2 - i));
-			mm[i].sc = letters[i % 3];
-			mm[i].sq = i + 1;
-		}
+void random( mon mm[]) {
+    for (int n = 0; n < M; n++) {
+        printf("%d. Введите название>", n + 1);
+        scanf_s("%s", mm[n].name, sizeof(mm[n].name));
+    }
+    for (int n = 0; n < M; n++) {
+        printf("%d. Введите ариал>", n + 1);
+        scanf_s("%s", mm[n].cnt, sizeof(mm[n].cnt));
+    }
+    char letters[3] = { 'A', 'B' ,'H' };
+    for (int i = 0; i < 3; i++)
+    {
+        mm[i].sc = letters[i % 3];
+        mm[i].sq = rand() % 557501;
+    }
+    mm[10] = mm[2];
+    mm[2] = mm[0];
+    mm[0] = mm[10];
 
-		mm[10] = mm[2];
-		mm[2] = mm[0];
-		mm[0] = mm[10];
-	
 }
 int main(void)
 {
-	srand(time(0));
-	setlocale(LC_ALL, "Russian");
-	int choice = 0;
-	int n = 3;
-	float sqx;
-	mon mm[M];
-	while (true) {
-		for (int i = 0; i < 1; i++)
-		{
-			cout << "Выберите действие (Ввод с экрана - 1/Заполнение случайным образом - 2/Сортировка - 3/Печать - 4):" << endl;
-			cin >> choice;
-			if (choice == 1) {
-				input(mm);
-			}
-			else if (choice == 2) {
-				random(mm);
-			}
-			else if (choice == 3) {
-				sort(mm);
-			}
-			else if (choice == 4) {
-				print(mm);
-			}
-		}
-	}
-	return 0;
+    srand(time(0));
+    setlocale(LC_ALL, "Russian");
+    int choice = 0;
+    int n = 3;
+    float sqx;
+    struct mon mm[10];
+    while (true) {
+        cout << "Выберите операцию (Таблица рассчитана на 3 вида):" << endl;
+        cout << "(1)Заполнение рандомно" << endl;
+        cout << "(2)Ручной ввод" << endl;
+        cout << "(3)Сортировка по алфавиту" << endl;
+        cout << "(4)Печать" << endl;
+        cin >> choice;
+        if (choice == 1) {
+            random(mm);//как швейцарские часы
+        }
+        else if (choice == 2) {
+            input(mm);//???
+        }
+        else if (choice == 3) {
+            sort(mm);//correct
+        }
+        else if (choice == 4) {
+            print(mm);//исправно
+        }
+
+    }
+    
 }
