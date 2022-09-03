8f288bd660e5ac3eeddd40152fe4e5597ebd512a
Дар`я Автенюк <Daria.Avteniuk@cs.khpi.edu.ua>
2022-09-03 13:23:33 +0200

    d

--- Term_3/Lab_0A/Lab_0A/Lab_0A.cpp
+++ Term_3/Lab_0A/Lab_0A/Lab_0A.cpp
@@ -1,6 +1,4 @@
-﻿// Lab_0A.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-#include "Header.h"
+﻿#include "Header.h"
 #include <iostream>
 
 int main()
