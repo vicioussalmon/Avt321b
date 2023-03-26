eda1e0db3f8e64b173e3a3a1725da20e15ef82b1
vicioussalmon <Daria.Avteniuk@cs.khpi.edu.ua>
2023-03-26 01:31:08 +0100

    done

---
+++ CPP/Lab_1/Lab_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input your line");
+string phrase = Convert.ToString(Console.ReadLine());
+string[] words = phrase.Split(' ');
+for (int i = 0; i < words.Length; i++)
+{
+    if (words[i].Length <= 3)
+    {
+        words[i] = "";
+    }
+}
+for (int i = 0; i < words.Length; i++)
+{
+
+    Console.Write(words[i] + " ");
+}
